147a0e7898f754ad57fbbf26ed1cdc4c01b69c9a
Shrey <liveprosperdie7@gmail.com>
2026-03-27 18:24:18 +0530

    Add files via upload

---
+++ numb3rs.py
@@ -0,0 +1,16 @@
+import re 
+import sys
+
+def main():
+    print((validate(input("IPv4 Address: "))))
+
+
+def validate(ip):
+    num=re.search(r"^(\d+)\.(\d+)\.(\d+)\.(\d+)$", ip)
+    if num:
+        return all(0<=int(num.group(i))<=225 for i in range(1,5))
+    else:
+        return False
+
+if __name__=="__main__":
+    main()
